b3bb906e4771a27d889cec2803a724bf3e5acf17
parsonsmatt <parsonsmatt@gmail.com>
2018-08-07 19:31:45 -0600

    Migration utility

--- metricord-web/migrate/Main.hs
+++ metricord-web/migrate/Main.hs
@@ -1,20 +1,43 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
-import Prelude
+import ClassyPrelude
 
+import Data.Function (fix)
+import Options.Generic
 import Control.Concurrent
+import qualified Data.Text as Text
 
 import Metricord.Migrate
 
 main :: IO ()
 main = do
-    putStrLn "Running migrations . . ."
-    threadDelay 100000
-    putStr ". "
-    threadDelay 100000
-    putStr ". "
-    threadDelay 100000
-    putStr ". "
-    threadDelay 100000
-    putStrLn "er, not implemented yet!"
+    cmd <- getRecord "metricord-migrate"
+    case cmd of
+        New {..} -> do
+            let dir =
+                    fromMaybe "./migrations"
+                    . unHelpful
+                    $ directory
+            newMigration dir (unHelpful tableName) (unHelpful field)
+        Run {..} ->
+            putStrLn "Not implemented yet"
 
+    print cmd
+
+type DirectoryArg = Maybe FilePath <?> "The location of the SQL migrations. Defaults to ./migrations"
+
+data Cmd
+    = New
+        { directory :: DirectoryArg
+        , tableName :: Text <?> "The name of the table to create."
+        , field :: [MigrateColumn] <?> "The fields to seed the table with."
+        }
+    | Run
+        { directory :: DirectoryArg
+        }
+    deriving (Show, Generic)
+
+instance ParseRecord Cmd
